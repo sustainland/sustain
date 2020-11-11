4ffd7a5c42bca4b766e224d1110dc936500e452b
Aymen <labidi@aymen.co>
2020-11-11 18:44:14 +0100

    request-logger log with color

---
+++ packages/core/src/extensions/logger/request-logger.extension.ts
@@ -0,0 +1,28 @@
+import {SustainResponse} from './../../interfaces/SustainResponce.interface';
+import {SustainExtension} from './../../interfaces/IExtension.interface';
+import {Injectable} from './../../di/injectable';
+import {SustainRequest} from '../../interfaces';
+@Injectable()
+export class RequestLoggerExtension implements SustainExtension {
+  onResquestStartHook(request: SustainRequest) {
+    request.startAt = new Date();
+  }
+  onResponseEndHook(request: SustainRequest, response: SustainResponse) {
+    const endTime: any = new Date();
+    const timeDiff: any = endTime - request.startAt;
+    // switch for code error
+    let color;
+    if (response.statusCode < 400) { 
+      color = `\x1b[32m$$\x1b[0m `;
+    } else {
+      color = `\x1B[31m$$\x1b[0m`;
+    }
+    console.log(
+      color.replace('$$', `${request.method} ${request.url}`),
+      `${response.statusCode}`,
+      'in ',
+      timeDiff,
+      'ms'
+    );
+  }
+}
