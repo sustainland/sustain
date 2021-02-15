0712463038cc925735ee25173c2846690f54c081
Aymen <labidi@aymen.co>
2021-02-15 12:35:48 +0100

    add envirements

--- samples/jwt-authentication/src/app.ts
+++ samples/jwt-authentication/src/app.ts
@@ -1,9 +1,14 @@
+import {ENVIREMENT} from './modules/auth/constants';
 import {AuthModule} from './modules/auth/AuthModule';
 import {HelloService} from './services/HelloService';
 import {App, bootstrap} from '@sustain/core';
 import {SwaggerModule} from '@sustain/swagger';
 import BaseController from './controllers/BaseController';
-require('source-map-support').install();
+
+const {envirement} = require('@sustain/config');
+if (envirement == ENVIREMENT.DEVELOPMENT) {
+  require('source-map-support').install();
+}
 
 const bodyParser = require('@sustain/body-parser');
 @App({
--- samples/jwt-authentication/src/modules/auth/constants.ts
+++ samples/jwt-authentication/src/modules/auth/constants.ts
@@ -1,1 +1,5 @@
 export const JWT_ALGORITHM = 'HS256';
+export const ENVIREMENT = {
+  DEVELOPMENT: 'development',
+  PRODUCTION: 'production',
+};
