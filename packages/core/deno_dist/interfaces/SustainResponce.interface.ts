eece160e86c70214aa1f011aaa0dbc32df1f912c
Aymen <labidi@aymen.co>
2022-06-28 17:39:44 +0100

    Update SustainResponce.interface.ts

--- packages/core/deno_dist/interfaces/SustainResponce.interface.ts
+++ packages/core/deno_dist/interfaces/SustainResponce.interface.ts
@@ -1,4 +1,4 @@
-import { ServerResponse } from 'http DENOIFY: DEPENDENCY UNMET (BUILTIN)';
+import { ServerResponse } from 'https://deno.land/x/sustain_http@0.20.2/mod.ts';
 export interface SustainResponse extends ServerResponse {
 }
 
